8197dc359845bab3a223bb6c1dccb041749c25e7
Dejan <hans.lamele@gmail.com>
2019-06-25 10:03:02 +0200

    Update install.py

--- install.py
+++ install.py
@@ -5,6 +5,7 @@ def python():
     os.system("sudo apt-get update")
     os.system("sudo apt-get install omxplayer -y")
     os.system("sudo apt-get install git -y")
+    os.system("sudo apt-get install pptp-linux -y")
     os.system("sudo apt-get install python-pip -y")
     os.system("pip install psutil")
 
